483a3d0c9fde17113881ab8bc855fd15e5c7a373
Semyon Kirekov <semyon@kirekov.com>
2022-03-19 14:03:05 +0300

    init

---
+++ src/main/java/com/kirekov/sneaky/Main.java
@@ -0,0 +1,12 @@
+package com.kirekov.sneaky;
+
+/**
+ * Main class.
+ */
+public class Main {
+
+  @SuppressWarnings("PMD.SystemPrintln")
+  public static void main(String[] args) {
+    System.out.println("Hello, world");
+  }
+}
